c29eebd3d212879b88ab64884d1212f62c4b7f1c
小俣幸之助 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-07 13:04:10 +0900

    change to use status enum for network request

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -27,16 +27,18 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
+enum class MarsApiStatus {LOADING, ERROR, DONE}
+
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status String
-    val status: LiveData<String>
+    val status: LiveData<MarsApiStatus>
         get() = _status
 
     private val _properties = MutableLiveData<List<MarsProperty>>()
@@ -68,12 +70,15 @@ class OverviewViewModel : ViewModel() {
         coroutineScope.launch {
             val getPropertiesDeferred = MarsApi.retrofitService.getPropertiesDeferred()
             try {
+                _status.value = MarsApiStatus.LOADING
                 val listResult = getPropertiesDeferred.await()
-                if (listResult.isNotEmpty()) {
-                    _properties.value = listResult
-                }
+
+                _status.value = MarsApiStatus.DONE
+                _properties.value = listResult
+
             } catch (t: Throwable) {
-                _status.value = "Failure " + t.message
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = emptyList() // clear recyclerView when error.
             }
         }
         // callback version
